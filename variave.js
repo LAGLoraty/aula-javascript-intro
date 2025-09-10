87295d23ae873060dfea15f1800ad72e48152670
LAGLoraty <alex_loraty@estudante.sesisenai.org.br>
2025-09-10 19:39:18 -0300

    diferencias variaves portugol javascript

---
+++ variave.js
@@ -0,0 +1,26 @@
+// transição de Portugol para javascript
+//Declaração de variáveis
+
+/* Em portugol:
+cadeia nome = "Alex"
+cadeia sobrenome = "Galeano"
+inteiro idade = 29
+real altura = 1.83
+logico ehHumano = verdadeiro
+
+escreva("Nome: ", nome + sobrenome)
+escreva("Idade: ", idade)
+escreva("Altura: ", altrura)
+escreva("É humano? ", ehHumano)
+*/
+
+let nome = "Alex" // cadeia nome = "Alex"
+let sobrenome = "Galeano" // cadeia sobrenome = "Galeano"
+let idade = 29 // inteiro idade = 16
+let altura = 1.83 // real altura = 1.83
+let ehHumano = true // logico ehHumano = verdadeiro
+
+console.log("Nome: ", nome + sobrenome) // escreva("Nome: ", nome + sobrenome)
+console.log("Idade: ", idade) // escreva("Idade: ", idade)
+console.log("Altura: ", altura) // escreva("Altura: ", altrura)
+console.log("É humano? ", ehHumano) // escreva("É humano? ", ehHumano)
